3aeaf682fe117a1c12b078d44f79c5208ec147e9
felixogar <felixogar50@gmail.com>
2022-12-10 14:23:02 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
  * main - use the function puts to print a new line
- * return: 0
+ *
+ * Return: 0
 */
+
 int main(void)
 {
-	puts("\"programming is like building a multi lingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
