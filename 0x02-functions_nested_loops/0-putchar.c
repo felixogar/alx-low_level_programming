30094ccb05711d06fef30dd0c855ce9b1fe1ffb9
felixogar <felixogar50@gmail.com>
2022-12-14 14:48:24 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "felix.o"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('F');
+	_putchar('e');
+	_putchar('l');
+	_putchar('i');
+	_putchar('x');
+	_putchar('\n');
+	return (0);
+}
