e868dacadaa832e581a68c2c1b24e5963ba843cf
“felixogar” <felixogar50@gmail.com>
2022-12-17 02:07:57 +0100

    correction

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,30 +1,15 @@
 #include "main.h"
 
 /**
- * print_number - prints an arbitrary integer using putchar
+ * _isupper - return if c is uppercase
  *
- * @n: integer to print
+ * @c: integer to examine
  *
- * Return: void
+ * Return: 1 if upper, 0 otherwise
  */
-void print_number(int n)
+int _isupper(int c)
 {
-	unsigned int u, i;
-
-	if (n < 0)
-	{
-		u = -n;
-		_putchar('-');
-	}
-	else
-	{
-		u = n;
-	}
-
-	i = 1000000000;
-	do {
-		if (i <= u || i == 1)
-			_putchar(u / i % 10 + '0');
-		i /= 10;
-	} while (i != 0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
 }
